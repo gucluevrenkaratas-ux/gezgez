9b6993439c3e6b149d72f3a00a948fef36f6efb6
gucluevrenkaratas-ux <gucluevrenkaratas@gmail.com>
2026-04-25 03:46:08 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GezgezApp from "./GezgezApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GezgezApp />
+  </React.StrictMode>
+);
